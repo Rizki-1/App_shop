683e214500167dff3f2a424995805d297f04683a
Rizki-1 <143168532+Rizki-1@users.noreply.github.com>
2024-05-01 18:41:09 +0700

    web

--- resources/views/Login.blade.php
+++ resources/views/Login.blade.php
@@ -14,12 +14,34 @@
 <form action="{{ route('LoginStore') }}" method="post">
     @csrf
     <label for="email"></label>
-    <input type="email" name="email">
-    <input type="password" name="password">
-    <button type="submit">login</button>
+    <input type="email" name="email" id="email">
+    <input type="password" name="password" id="Password">
+    <input type="checkbox" onclick="ShowPassword()"> show password
+    <button type="button" onclick="check()" id="button">login</button>
 </form>
 <h2>
     tidak punya akun? <a href="{{ route('daftar') }}"> daftar di sini </a>
 </h2>
+<script>
+    function ShowPassword() {
+        var x = document.getElementById("Password");
+        if (x.type == "password") {
+            x.type = "text";
+        }else{
+            x.type = "password";
+        }
+    }
+    function check(){
+        var email = document.getElementById("email").value;
+        var password = document.getElementById("Password").value;
+        var button = document.getElementById("button");
+        if (email == "" || password == "" ) {
+            alert('email atau password belum di isi');
+        }else{
+            button.type = 'submit';
+            button.click();
+        }
+    }
+</script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -10,16 +10,17 @@ Route::get('/', [DashboardController::class, 'index'])->name('/');
 Route::get('login', [LoginController::class, 'index'])->name('login');
 Route::get('daftar', [LoginController::class, 'daftar'])->name('daftar');
 
-//server
+//login
 Route::post('DaftarStore', [LoginController::class, 'store'])->name('DaftarStore');
 Route::post('LoginStore', [LoginController::class, 'Loging'])->name('LoginStore');
 
-//account login
+//user login
 Route::prefix('/User')->middleware(['AuthMiddleware'])->group(function () {
 Route::post('Logout', [LoginController::class, 'Logout'])->name('logout');
 Route::get('Home', [UserController::class, 'index'])->name('userHome');
 });
 
+//admin login
 Route::prefix('SuperAdmin')->middleware(['SuperAdminMiddleware'])->group(function () {
     Route::get('Home', [SuperAdminController::class, 'index'])->name('SuperAdminHome');
 });
